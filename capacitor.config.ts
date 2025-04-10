c76b9a27125ccfab116ce2c2523488ad9d6f0e4c
Mylena Roberta <m222687@dac.unicamp.br>
2025-04-10 11:00:01 -0300

    feat: initial boilerplate

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+	appId: 'br.unicamp.ic.saude',
+	appName: 'saude',
+	webDir: 'dist',
+};
+
+export default config;
