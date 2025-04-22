fc8c16d1e89241fe29d0be2b98c18dbca8f5c639
Jmvjr <j219255@dac.unicamp.br>
2025-04-22 11:38:23 -0300

    first AcsMainPage

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import ComponentCatalog from './pages/ComponentCatalog';
 import UserOnboarding from './pages/UserOnBoarding';
+import AcsMainPage from './pages/AcsMainPage';
 
 const router = createBrowserRouter([
   {
@@ -10,6 +11,10 @@ const router = createBrowserRouter([
   {
     path: '/components',
     element: <ComponentCatalog />,
+  },
+  {
+    path: '/AcsMainPage',
+    element: <AcsMainPage />,
   },
   {
     path: '/forms-user-1',
