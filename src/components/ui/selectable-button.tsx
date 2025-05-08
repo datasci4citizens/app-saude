5c4169b43d0b41a72afc6bc0f57bc18384cf2a03
Eduardo Migueis <edmigueis@hotmail.com>
2025-05-08 19:49:26 -0300

    fix: visual ug fixed

--- src/components/ui/selectable-button.tsx
+++ src/components/ui/selectable-button.tsx
@@ -20,11 +20,11 @@ export const SelectableOption: React.FC<SelectableOptionProps> = ({
         <div className="relative flex-shrink-0">
           <div
             className={`w-6 h-6 rounded-full border ${
-              selected ? "border-dark_blue border-2" : "border-dark_blue"
+              selected ? "border-[#141B36] border-2" : "border-[#141B36]"
             }`}
           >
             {selected && (
-              <div className="absolute w-5 h-5 rounded-full bg-lime top-[50%] left-[50%] transform -translate-x-1/2 -translate-y-1/2" />
+              <div className="absolute w-5 h-5 rounded-full bg-lime top-[50%] left-[50%] transform -translate-x-1/2 -translate-y-1/2 bg-[#ddfc8e]" />
             )}
           </div>
         </div>
