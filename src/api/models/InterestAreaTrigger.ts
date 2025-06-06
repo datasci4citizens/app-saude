0db233adc32d854f77f11adbbf7c9a7f0a460603
Jun <gustavogujun@gmail.com>
2025-06-06 03:18:59 -0300

    chore: regenerate api

---
+++ src/api/models/InterestAreaTrigger.ts
@@ -0,0 +1,11 @@
+/* generated using openapi-typescript-codegen -- do not edit */
+/* istanbul ignore file */
+/* tslint:disable */
+/* eslint-disable */
+export type InterestAreaTrigger = {
+    observation_concept_id?: number | null;
+    trigger_name?: string | null;
+    value_as_string?: string | null;
+    concept_name?: string | null;
+};
+
